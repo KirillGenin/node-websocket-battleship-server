6ace6453846002627b2f5b00a9aa976e703e6575
KirillGenin <332211op@gmail.com>
2023-07-07 15:51:33 +0300

    chore: add callback for 'listening' event for ws server

--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -1,8 +1,12 @@
 import { WebSocketServer } from 'ws';
 import { handlerConnection } from './handlers/connection';
 
-export const createWebSocketServer = (port: number, cb: () => void) => {
+export const createWebSocketServer = (port: number) => {
   const wss = new WebSocketServer({ port });
+
+  wss.on('listening', () => {
+    console.log(`Start websocket server on the ${port} port!`);
+  });
+
   wss.on('connection', handlerConnection);
-  cb();
 };
