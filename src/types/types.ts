d72e9a0c1cba4bdfb6ccc27ba0e83f52bc3ad5f1
KirillGenin <332211op@gmail.com>
2023-07-07 15:49:00 +0300

    chore: add enums for game events, ship types, attack status, interfaces for request and response for registration user

---
+++ src/types/types.ts
@@ -0,0 +1,55 @@
+/* ENUM */
+
+export enum GameEvent {
+  REG = 'reg',
+  UPDATE_WINNERS = 'update_winners',
+  CREATE_ROOM = 'create_room',
+  ADD_USER_TO_ROOM = 'add_user_to_room',
+  CREATE_GAME = 'create_game',
+  UPDATE_ROOM = 'update_room',
+  ADD_SHIPS = 'add_ships',
+  START_GAME = 'start_game',
+  ATTACK = 'attack',
+  RANDOMATTACK = 'randomAttack',
+  TURN = 'turn',
+  FINISH = 'finish',
+}
+
+export enum ShipType {
+  SMALL = 'small',
+  MEDIUM = 'medium',
+  LARGE = 'large',
+  HUGE = 'huge',
+}
+
+export enum AttackStatus {
+  MISS = 'miss',
+  KILLED = 'killed',
+  SHOT = 'shot',
+}
+
+/* REQUEST */
+
+export interface RequestMessage {
+  type: string;
+  data: string;
+}
+
+export interface RequestDataReg {
+  name: string;
+  password: string;
+}
+
+/* RESPONSE */
+
+export interface ResponseDataReg {
+  name: string;
+  index: number;
+  error: boolean;
+  errorText: string;
+}
+
+export interface ResponseReg {
+  type: GameEvent.REG;
+  data: ResponseDataReg;
+}
